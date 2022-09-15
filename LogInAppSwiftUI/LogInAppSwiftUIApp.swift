041e6bd4badd4780b3619ac0dc83d0aac4614ade
JohnyLearnSwift <eugenietyan@gmail.com>
2022-09-15 16:30:03 +0300

    Initial Commit

---
+++ LogInAppSwiftUI/LogInAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogInAppSwiftUIApp.swift
+//  LogInAppSwiftUI
+//
+//  Created by Eugenie Tyan on 15.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct LogInAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
